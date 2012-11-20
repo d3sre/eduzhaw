65adcba467566028e436605c92bd151f29a5be48
Desiree Sacher <sachedes@students.zhaw.ch>
2012-11-20 11:39:44 +0100

    description of intention/idea

---
+++ strategyDemo/src.main.java/ICar.java
@@ -0,0 +1,22 @@
+package src.main.java;
+
+public class ICar {
+	
+	/**
+	 *  Interface fuer Car, Auto hat Features und unterschiedliche Verhaltensweisen im Sommer und im Winter, diese sollen
+	 *  je nach Jahreszeiten automatisch aktiv werden 
+	 */
+	
+	/**
+	 * Winter: bessere Pneu für Schnee/Regen
+	 * Bremsweg angepasst
+	 * Heizung funktioniert
+	 * Licht permament an da dunkel
+	 * 
+	 * Sommer: Sommerpneu
+	 * Bremsweg angepasst
+	 * Heizung auf Klimaanlage
+	 * Licht nur an wenn Sensor auf dunkel //ist ja nur Bsp Szenario auf die Schnelle, noch nicht ausgereift..
+	 */
+
+}
--- strategyDemo/src.main.java/StrategyImplementationMain.java
+++ strategyDemo/src.main.java/StrategyImplementationMain.java
@@ -1,5 +1,9 @@
 package src.main.java;
 
 public class StrategyImplementationMain {
+	
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
 
 }
